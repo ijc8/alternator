6c07392ff4b59680ca867e7c477eee38eb3fb0e1
Ian Clester <ijc@ijc8.me>
2021-09-17 00:40:24 -0400

    Clean up new Aleatora worker.

--- worker2.js
+++ worker2.js
@@ -1,56 +1,11 @@
-console.log("Web Worker: start")
-let port
+// Actual user (composer) code in Python:
+const composition = `
+from aleatora import *
 
-self.onmessage = async (event) => {
-    // Receive sample rate and Audio Worklet's port from main thread.
-    console.log("Web Worker: setup")
-    await setup(event.data.sampleRate)
-    port = event.data.port
-    port.onmessage = (e) => {
-        // Received empty buffer from Audio Worklet. Fill with generated samples and send it back.
-        // Uncomment to force occasional underruns:
-        // for (let j = 0; j < (1 << 25); j++) {}
-        const length = _process(e.data)
-        if (length < e.data.length) {
-            // `process` did not fill the buffer, indicating the end of the piece.
-            e.data.fill(0, length)
-            // Generate silence from here on out.
-            port.onmessage = (e) => {
-                e.data.fill(0)
-                port.postMessage(e.data, [e.data.buffer])
-            }
-        }
-        port.postMessage(e.data, [e.data.buffer])
-    }
-}
+main = osc(200)[:2.0] >> osc(400)[:2.0]
+`
 
-// --- User (composer) code starts here ---
-// let sampleRate
-// const freq = 300  // Hz
-// const dur = 2.5   // seconds
-// let phase, remainingSamples
-
-// function setup(_sampleRate) {
-//     sampleRate = _sampleRate
-//     phase = 0
-//     remainingSamples = Math.floor(dur * sampleRate)
-// }
-
-// function _process(output) {
-//     let i
-//     const n = Math.min(remainingSamples, output.length)
-//     for (i = 0; i < n; i++) {
-//         output[i] = Math.sin(phase)
-//         phase += 2*Math.PI*freq/sampleRate
-//         phase %= 2*Math.PI
-//     }
-//     remainingSamples -= i
-//     return i
-// }
-
-
-
-
+// Infrastructure:
 importScripts('pyodide/pyodide.js')
 
 async function setupPyodide() {
@@ -74,9 +29,6 @@ const setupPyodidePromise = setupPyodide()
 // returns a coroutine if `source` contains a top-level await, and None otherwise.
 
 const setupCode = `
-import ast
-import js
-import pyodide
 import sys
 
 # HACK: Shim "sounddevice" for aleatora.
@@ -85,23 +37,6 @@ import types
 sounddevice = types.ModuleType("sounddevice")
 sounddevice.query_devices = lambda: print("* 0 Audio Worklet Output Buffer")
 sys.modules["sounddevice"] = sounddevice
-
-async def run(source):
-    await js.pyodide.loadPackagesFromImports(source)
-    code = compile(source, "<string>", "exec", ast.PyCF_ALLOW_TOP_LEVEL_AWAIT)
-    globals = {}
-    result = eval(code, globals)
-    if result:
-        await result
-    stream = globals["main"]
-    print("Playing:", stream)
-    samples = iter(stream)
-    def callback(outdata, frames):
-        i = -1
-        for i, sample in zip(range(frames), samples):
-            outdata[i] = sample
-        return i + 1
-    return callback
 `
 
 let playing = true
@@ -111,16 +46,54 @@ async function setup(sampleRate) {
     await setupPyodidePromise
     // TODO: Cleaner way to do this?
     self.pyodide.runPython(`import aleatora.streams.audio; aleatora.streams.audio.SAMPLE_RATE = ${sampleRate}`)
-    // Actual user (composer) code:
-    self.pyodide.globals.set("source", `
-from aleatora import *
-
-main = osc(200)[:2.0] >> osc(400)[:2.0]
+    self.pyodide.runPython(composition)
+    audioCallback = self.pyodide.runPython(`
+print("Playing:", main)
+samples = iter(main)
+def callback(outdata, frames):
+    i = -1
+    for i, sample in zip(range(frames), samples):
+        outdata[i] = sample
+    return i + 1
+callback
 `)
-    audioCallback = await self.pyodide.runPython("run(source)")
+    console.log("Setup done")
 }
 
 // Temporary name change due to pyodide bug: https://github.com/pyodide/pyodide/issues/1846
 function _process(output) {
     return audioCallback(output, output.length)
+}
+
+
+// Even more basic infrastructure:
+console.log("Web Worker: start")
+let port
+
+function playSilence() {
+    port.onmessage = (e) => {
+        e.data.fill(0)
+        port.postMessage(e.data, [e.data.buffer])
+    }
+}
+
+self.onmessage = async (event) => {
+    // Receive sample rate and Audio Worklet's port from main thread.
+    console.log("Web Worker: setup")
+    port = event.data.port
+    playSilence()
+    setup(event.data.sampleRate).then(() => {
+        port.onmessage = (e) => {
+            // Received empty buffer from Audio Worklet. Fill with generated samples and send it back.
+            // Uncomment to force occasional underruns:
+            // for (let j = 0; j < (1 << 25); j++) {}
+            const length = _process(e.data)
+            if (length < e.data.length) {
+                // `process` did not fill the buffer, indicating the end of the piece.
+                e.data.fill(0, length)
+                playSilence()
+            }
+            port.postMessage(e.data, [e.data.buffer])
+        }
+    })
 }
