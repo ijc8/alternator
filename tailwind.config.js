41dff2a28c4026f957d976994a49c2ec60bb9a7c
Ian Clester <ijc@ijc8.me>
2021-09-26 19:10:24 -0400

    Enable Tailwind JIT.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
