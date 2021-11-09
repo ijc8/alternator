e039eaa22a1c53c238767c70d4571b35e63a03cd
Ian Clester <ijc@ijc8.me>
2021-11-09 09:59:23 -0500

    Interleaved follow-up.

--- compositions/c-stereo-test/main.c
+++ compositions/c-stereo-test/main.c
@@ -14,8 +14,8 @@ void setup(int _samplerate) {
 EMSCRIPTEN_KEEPALIVE
 int process(float *output, int frames) {
     for (int i = 0; i < frames; i++, t++) {
-        output[i] = sinf(2*M_PI*200.f/samplerate*t);
-        output[frames + i] = sinf(2*M_PI*300.f/samplerate*t);
+        output[i * 2] = sinf(2*M_PI*200.f/samplerate*t);
+        output[i * 2 + 1] = sinf(2*M_PI*300.f/samplerate*t);
     }
     return frames;
 }
