71e1e7042f4d30d983ef2150022b47af92c781e6
Ian Clester <ijc@ijc8.me>
2022-10-24 11:36:22 -0400

    Choose random frequency in Rust Wasm example

--- wasm/rust-wasm-export/src/lib.rs
+++ wasm/rust-wasm-export/src/lib.rs
@@ -1,5 +1,8 @@
+use rand::prelude::*;
+
 const N: usize = 1024;
 
+static mut freq: f64 = 0.0;
 static mut phase: f64 = 0.0;
 static mut sample_rate: f64 = 44100.0;
 #[no_mangle]
@@ -8,13 +11,15 @@ static mut buf: [f32; N] = [0.0; N];
 #[no_mangle]
 pub unsafe fn setup(_sample_rate: f32) -> *const f32 {
     sample_rate = _sample_rate as f64;
+    freq = rand::thread_rng().gen_range(220.0..880.0);
     buf.as_ptr()
 }
 
 #[no_mangle]
-pub unsafe fn process() {
+pub unsafe fn process() -> i32 {
     for x in buf.iter_mut() {
         *x = phase.sin() as f32;
-        phase += std::f64::consts::TAU * 200.0 / sample_rate;
+        phase += std::f64::consts::TAU * freq / sample_rate;
     }
+    N as i32
 }
